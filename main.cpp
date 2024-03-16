4f524f035cb4f8216f0a9581ccbef4abdb1ecb5e
Vojago <Test@vojago.com>
2024-03-16 17:57:34 +0800

    完成维吉尼亚密码

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "head.h"
 int main() {
     std::cout << "Hello, CLion!" << std::endl;
-//    caesar_main();
-    vigenere_main();
+    caesar_main();//凯撒密码
+    vigenere_main();//维吉尼亚密码
     return 0;
 }
